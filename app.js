2219abae8ef7f1c878430060a060b388870e4f59
Flammrock <flammrockgame@gmail.com>
2020-03-30 08:18:25 +0200

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 
 
 const TOKEN = 'NjkzODI1MzM0ODM1MTUwOTE4.XoC3CQ.meL6PnRHcv91pS2xnyRytJ3oiZE';
-const DATABASE_URI = process.env.DATABASE_URL;
+const DATABASE_URI = process.env.CLEARDB_DATABASE_URL;
 const DATABASE_PARSE = DATABASE_URI.match(/mysql:\/\/([^:]+):([^@]+)@([^\/]+)\/([^\?]+)\??/);
 
 console.log(DATABASE_PARSE);
