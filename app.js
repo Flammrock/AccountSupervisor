5c1b403cf91070a2a41c09f42996ba53a692f7e3
Flammrock <flammrockgame@gmail.com>
2020-03-30 05:16:11 +0200

    update

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ class ParserCommand {
 
 // ADMIN
 new Command('ping', function(msg,args) {
-	if (!(message.member.roles.cache.some(r => r.name === "BankAdmin") || msg.member.hasPermission("ADMINISTRATOR"))) {
+	if (!(msg.member.roles.cache.some(r => r.name === "BankAdmin") || msg.member.hasPermission("ADMINISTRATOR"))) {
 		msg.delete(0);
 		msg.author.send('Sorry, you don\'t have the permissions :cold_sweat:\nAnd i\'ve decided to delete your message.');
 		return;
