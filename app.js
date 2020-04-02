1adb4d5af7b4e0c3ff8b979010528f62199a69d4
Flammrock <flammrockgame@gmail.com>
2020-04-02 09:50:48 +0200

    update

--- app.js
+++ app.js
@@ -2537,7 +2537,7 @@ function activities() {
 	var time = Date.now();
 	var tick = getRandomIntInclusive(10, 30)*1000;
 	
-	bot.user.setStatus('dnd', 'Made by KwinkyWolf');
+	bot.user.setStatus('dnd');
 	
 	setInterval(() => {
 		if (Date.now()-time<tick) return;
